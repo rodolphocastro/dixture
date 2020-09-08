817d058537058a0f27f27423504780fc64106dd2
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-09-07 22:41:54 -0300

    feat: Add field assignment functionality
    This commit is a combination of:
    
    + feat: Add bigint generation
    + feat: Add method to apply fields to an existing object
    + tests: Enhance createField Test Suite
    + tests: Create negative test cases
    + chore: Publicly export the applyField function

--- src/_simpleGenerator.ts
+++ src/_simpleGenerator.ts
@@ -14,11 +14,17 @@ function generateRandomNumber(
   return (Math.random() * max) + min;
 }
 
+/**
+ * Creates a random value for a string, boolean, number or bigint field.
+ * @param subject object that owns the field
+ * @param field the field itself
+ * @param params parameters for random number generation
+ */
 export function createFieldValue<T>(
   subject: T,
   field: keyof T,
   params: GeneratorParameters = defaultParameters,
-): string | boolean | number {
+): string | boolean | number | bigint {
   const suffix = generateRandomNumber(params);
   const typeOfField = typeof subject[field];
   switch (typeOfField) {
@@ -28,7 +34,24 @@ export function createFieldValue<T>(
       return Math.random() >= .5;
     case "number":
       return suffix;
+    case "bigint":
+      return BigInt(Math.floor(suffix));
     default:
       throw new Error(`${typeOfField} is not supported`);
   }
+}
+
+/**
+ * Creates and assign a value for a string, boolean, number or bigint field.
+ * @param subject object that owns the field
+ * @param field the field itself
+ * @param params parameters for random number generation
+ */
+export function assignField<T>(
+  subject: T,
+  field: keyof T,
+  params: GeneratorParameters = defaultParameters,
+) {
+  const fieldValue = createFieldValue(subject, field, params);
+  (subject[field] as any) = fieldValue;
 }
