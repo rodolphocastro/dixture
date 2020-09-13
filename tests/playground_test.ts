0aef0f6df1c41eb9353d0922743ab3685d2287c5
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-09-13 11:31:59 -0300

    wip: Attempt #1 to build an interface wrapper...

--- tests/playground_test.ts
+++ tests/playground_test.ts
@@ -9,12 +9,33 @@ interface JustAnotherSubject {
 
 // Yeah, this is copy and paste. Sorry folks
 
+// TODO: Move RuleFor<T, K> into a shared .ts so both interfaces and class can leverage it
 /**
  * Describes a rule for a field of an interface.
  */
 interface RuleFor<T, K extends keyof T> {
   field: K;
   resolve: (...args: any[]) => T[K];
+}
+
+// interface InterfaceWrapper<T> {
+//   subjectKey: (keyof T)[];
+// }
+
+/**
+ * Nicely wraps an interface so users won't have to bother with magic strings :)
+ */
+class InterfaceWrapper<T> {
+  protected readonly interfaceKeys: string[];
+
+  // TODO: Might be safer to do a hash here?
+  get identifier() {
+    return this.interfaceKeys.join("|");
+  }
+
+  constructor(...keys: (keyof T)[]) {
+    this.interfaceKeys = keys.map((k) => k.toString());
+  }
 }
 
 /**
@@ -31,6 +52,9 @@ export class InterfaceBlueprint<T> {
    */
   protected readonly rules: RuleFor<T, keyof T>[];
 
+  // TODO: I'm not fond of passing the interface name as a "open" string. I'll try to get rid of this :)
+  // However I'll still need the name if I wish to plug this into the Dixture factory... I'll need to get some more
+  // prototypes for this before publishing a public api :/
   constructor(
     protected readonly interfaceName: string,
     ...args: RuleFor<T, keyof T>[]
@@ -78,6 +102,22 @@ Rhum.testPlan(
             assert(result.name === "Just Another Brick in the Wall");
             console.log(result.name);
             console.log(result.goodThru);
+          },
+        );
+      },
+    );
+
+    Rhum.testSuite(
+      "2. Interface Wrappers",
+      () => {
+        Rhum.testCase(
+          "1. Should extract the keys from the interface and expose them as identifiers",
+          () => {
+            const subject = new InterfaceWrapper<JustAnotherSubject>(
+              "goodThru",
+              "name",
+            );
+
           },
         );
       },
